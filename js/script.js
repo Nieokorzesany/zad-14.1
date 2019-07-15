67c39b58c07ad4e679b2e1679ea864b6927ffebb
Piotr Wlazeł <pancu1222@gmail.com>
2019-07-15 15:42:18 +0200

    add carousel

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var elem = document.querySelector(".main-carousel");
+var flkty = new Flickity(elem, {
+  // options
+  cellAlign: "center",
+  contain: true
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity(".main-carousel", {
+  // options
+});
+
+var restartGroup = document.querySelectorAll(".restart");
+restartGroup.forEach(button => {
+  button.addEventListener("click", function() {
+    flkty.select(0);
+  });
+});
+
+var progressBar = document.querySelector(".progress-bar");
+
+flkty.on("scroll", function(progress) {
+  progress = Math.max(0, Math.min(1, progress));
+  progressBar.style.width = progress * 100 + "%";
+});
